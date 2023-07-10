f1ba4ee4491dcb88e15a51d3ddb908bc22c248d0
UKMeng <ukmengse@gmail.com>
2023-07-10 11:22:30 +0800

    ✨ feat(Casks): Add Playcover.app v3.0.0-beta.1

---
+++ Casks/playcover-nightly.rb
@@ -0,0 +1,24 @@
+cask "playcover-nightly" do
+  version "3.0.0-beta.1"
+  sha256 :no_check
+
+  url "https://github.com/PlayCover/PlayCover/releases/download/#{version}/PlayCover_#{version}.dmg"
+  name "PlayCover"
+  desc "Sideload iOS apps and games"
+  homepage "https://github.com/PlayCover/PlayCover"
+
+  auto_updates true
+  depends_on arch: :arm64
+  depends_on macos: ">= :monterey"
+
+  app "PlayCover.app"
+
+  zap trash: [
+    "~/Library/Application Support/io.playcover.PlayCover",
+    "~/Library/Caches/io.playcover.PlayCover",
+    "~/Library/Containers/io.playcover.PlayCover",
+    "~/Library/Frameworks/PlayTools.framework",
+    "~/Library/Preferences/io.playcover.PlayCover.plist",
+    "~/Library/Saved Application State/io.playcover.PlayCover.savedState",
+  ]
+end
